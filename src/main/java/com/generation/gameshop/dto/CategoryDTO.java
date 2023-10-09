78ad65276f8b27f83a1bd4d99d58328d3714d196
Elias Moura <elias.smourayt@gmail.com>
2023-10-09 17:03:01 -0300

    Alterando DTO para fazer mais sentido na aplicação.

--- src/main/java/com/generation/gameshop/dto/CategoryDTO.java
+++ src/main/java/com/generation/gameshop/dto/CategoryDTO.java
@@ -1,11 +1,20 @@
 package com.generation.gameshop.dto;
 
+import com.generation.gameshop.model.Category;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
-public record CategoryData(
+public record CategoryDTO(
+        @NotNull
+        Long id,
         @NotBlank(message = "O campo 'name' é obrigatório.")
         @Size(min = 2, max = 100, message = "O campo 'name' não deve conter entre 2 a 100 caracteres.")
         String name
 ) {
+
+        public CategoryDTO(Category category) {
+                this(category.getId(), category.getName());
+        }
+
 }
