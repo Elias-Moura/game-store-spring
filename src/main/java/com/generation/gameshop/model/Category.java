b241c03b669cec0fd95010b9efa41d4b496c5de5
Elias Moura <elias.smourayt@gmail.com>
2023-10-09 17:03:38 -0300

    Adicionando delete lógico.

--- src/main/java/com/generation/gameshop/model/Category.java
+++ src/main/java/com/generation/gameshop/model/Category.java
@@ -1,27 +1,42 @@
 package com.generation.gameshop.model;
 
-import com.generation.gameshop.dto.CategoryData;
+import com.generation.gameshop.dto.CategoryDTO;
+import com.generation.gameshop.dto.CreateCategoryDTO;
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@Entity
+@Entity(name = "tb_categories")
 @Table(name = "tb_categories")
 public class Category {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
+    private Boolean active;
+    private Timestamp deleted_at;
 
-    Category(CategoryData data){
+    public Category(CreateCategoryDTO data){
+        this.active = true;
         this.name = data.name();
+    }
+
+    public Category(CategoryDTO data){
+        this.id = data.id();
+        this.name = data.name();
+    }
+
+    public void delete(){
+        this.active = false;
+        this.deleted_at = Timestamp.valueOf(LocalDateTime.now());
     }
 }
